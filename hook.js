c713be81b75a270a59036ac4ed9fe3a93bf31229
ReFormationPro <44122582+ReFormationPro@users.noreply.github.com>
2019-08-02 16:27:01 +0300

    initial

---
+++ hook.js
@@ -0,0 +1,53 @@
+// Only responseText and response properties are hooked.
+var _XMLHttpRequest = XMLHttpRequest;
+XMLHttpRequest = class XMLHttpRequest extends _XMLHttpRequest {
+    constructor() {
+        super();
+        this.hookResponseText();
+        this.hookResponse();
+    }
+    get_response() {
+        return this.responseIntercept();
+    }
+    get_responseText() {
+        return this.responseIntercept();
+    }
+    responseIntercept() {
+        delete this.response;   //Remove getter
+        var modifiedResponse = XMLHttpRequest.responseModify(this.response);    //Access self.response w/o getter (due to a weird situation caused by native code)
+        this.hookResponse(); //Add getter again
+        return modifiedResponse;
+    }
+    hookResponse() {
+        Object.defineProperty(this, "response", {
+            get: this.get_response,
+            configurable: true
+        });
+    }
+    hookResponseText() {
+        Object.defineProperty(this, "responseText", {
+            get: this.get_responseText,
+            configurable: true
+        });
+    }
+    /*
+     *  You are supposed to override this function to modify response.
+     */
+    static responseModify(resp) {
+        return resp;
+    }
+}
+
+XMLHttpRequest.responseModify = function(resp) {
+    try {
+        var json = JSON.parse(resp);
+        if (json.messages) {
+            json.messages.forEach(m => m.text = "I am so cool");
+            resp = JSON.stringify(json);
+        }
+        return resp;
+    } catch (e) {
+        return resp;
+    }
+}
+
